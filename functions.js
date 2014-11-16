fb1b27923b6bae4846a7f4d278ca5e47bde1c692
Rosario B. <rossa_4057@hotmail.com>
2014-11-16 02:33:50 +0100

    Formatting Date and Time

--- functions.js
+++ functions.js
@@ -105,10 +105,10 @@ function fillTblLessons(class_id, week_nr, year) {
 			//table body
 			$.each(data, function(key, value) {
 				html += '<tr>';
-				html += '	<td>' + value['tafel_datum'] + '</td>';
-				html += '	<td>' + value['tafel_wochentag'] + '</td>';
-				html += '	<td>' + value['tafel_von'] + '</td>';
-				html += '	<td>' + value['tafel_bis'] + '</td>';
+				html += '	<td>' + getFormattedDate(value['tafel_datum']) + '</td>';
+				html += '	<td>' + getWeekdayName(parseInt(value['tafel_wochentag'])) + '</td>';
+				html += '	<td>' + getFormattedTime(value['tafel_von']) + '</td>';
+				html += '	<td>' + getFormattedTime(value['tafel_bis']) + '</td>';
 				html += '	<td>' + value['tafel_lehrer'] + '</td>';
 				html += '	<td>' + value['tafel_longfach'] + '</td>';
 				html += '	<td>' + value['tafel_raum'] + '</td>';
@@ -122,6 +122,55 @@ function fillTblLessons(class_id, week_nr, year) {
 			alert('Error in lessons!');
 		}
 	});
+}
+
+function getFormattedDate(date) {
+	var d = new Date(date);
+	var month = parseInt(d.getMonth()) + 1;
+	return prependZeros(d.getDate(), 2) + '.' + prependZeros(month, 2) + '.' + d.getFullYear();
+}
+
+function getFormattedTime(time) {
+	var dateString = '2014-01-01 ' + time;
+	
+	var t = new Date(dateString);
+	var hours = parseInt(t.getHours());
+	var min = parseInt(t.getMinutes());
+	return prependZeros(hours, 2) + ':' + prependZeros(min, 2);
+}
+
+function prependZeros(number, length) {
+	var nString = number.toString();
+	var n = '';
+	
+	if(nString.length  < length) {
+		for(i = 0; i < (length - nString.length); i++) {
+			n += '0';
+		}
+	}
+	
+	return n + nString;
+}
+
+function getWeekdayName(number) {
+	switch(number) {
+		case 1:
+			return 'Montag';
+		case 2:
+			return 'Dienstag';
+		case 3:
+			return 'Mittwoch';
+		case 4:
+			return 'Donnerstag';
+		case 5:
+			return 'Freitag';
+		case 6:
+			return 'Samstag';
+		case 7:
+			return 'Sonntag';
+		default:
+			 return '';
+	}
 }
 
 function getTotalWeeksOfYear(year) {
